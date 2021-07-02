baba12287f0bfd9b4cf65f052704faeb7f07d432
tomo <krote0924@gmail.com>
2021-07-02 23:17:02 +0900

    typescript

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,6 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+const handler = (_, res) => {
+  const body = {message: "Hello"};
+  res.statusCode = 200;
+  res.json(body);
+}
+export default handler;
+/*
 export default (req, res) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
+*/
+
