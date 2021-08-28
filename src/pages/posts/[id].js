c0bf3761c177c312b72bf5e3371da8f1069f2d44
tomo <krote0924@gmail.com>
2021-08-28 13:26:06 +0900

    DynamicRoutes-5

--- src/pages/posts/[id].js
+++
@@ -1,9 +0,0 @@
-import { getAllPostIds } from '../../lib/posts'
-
-export async function getStaticPaths(){
-    const paths = gtAllPostIds()
-    return {
-        params,
-        fallback: false
-    }
-}
