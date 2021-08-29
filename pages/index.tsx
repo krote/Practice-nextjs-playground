84a886e1a71fac9aab5c4b191a49dbbdf82c0eab
tomo <krote0924@gmail.com>
2021-08-30 00:52:21 +0900

    finished tutorial

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,13 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
+  console.log("aaaa:")
+  console.log(allPostsData)
   return {
     props: {
       allPostsData
@@ -28,13 +32,16 @@ export default function Home({allPostsData}) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
+          <text>{allPostsData.count}</text>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br/>
-              {id}
-              <br/>
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
